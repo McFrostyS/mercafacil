7220f83dbdf6c293c1c2495119c6748639fe7396
Sergio Aguado Garrido <saguado9@gmail.com>
2024-07-25 23:21:43 +0200

    fix(ProductCard): show all info

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,14 +1,17 @@
 export const ProductCard = ({ result }) => {
   return (
-    <div className='card' data-key={result.id}>
+    <div key={result.id} className='card'>
       <img src={result.thumbnail} alt={result.displayName} className='card-img-top' />
       <div className='card-body'>
         <h5 className='card-title'>{result.displayName}</h5>
         <p className='brand-title'>{result.brand}</p>
-        <p className='previous-price'>{result.previousPrice}</p>
-        <p className='price'>{result.price}</p>
-        <p className='reference-price'>{result.referencePrice}</p>
-        <p className='reference-format'>{result.referenceFormat}</p>
+        <p className='card-text'>
+          Precio: <s>{result.previousPrice}</s> {result.price} €
+        </p>
+        <p>
+          ({result.referencePrice} €/
+          {result.referenceFormat.toLowerCase()})
+        </p>
       </div>
     </div>
   )
